7b70a8f4b6d5bca7ed6d7069b17c305086d3db6e
imakshug <akarni213gmail.com>
2024-05-28 15:06:48 +0530

    done

--- js/script.js
+++ js/script.js
@@ -314,28 +314,3 @@ Related works :
 Portforio design @wakana-k - https://codepen.io/wakana-k/pen/BaxKKvE
 Swaying photo gallery - hover event @wakana-k - https://codepen.io/wakana-k/pen/oNJxbPw
 */
-"use strict";
-(function () {
-	window.onload = () => {
-		const obj = document.querySelector("#gallery");
-		const time = 10000;
-		function animStart() {
-			if (obj.classList.contains("active") == false) {
-				obj.classList.add("active");
-				setTimeout(() => {
-					animEnd();
-				}, time);
-			}
-		}
-		function animEnd() {
-			obj.classList.remove("active");
-			obj.offsetWidth;
-		}
-		document.addEventListener("scroll", function () {
-			// scroll or scrollend
-			animStart();
-		});
-		window.addEventListener("resize", animStart);
-		animStart();
-	};
-})();
