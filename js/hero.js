360d67c4043004639cd2b08e7c794770805e9dd6
i-shivii <choureyshivi02@gmail.com>
2024-05-31 18:13:20 +0530

    done

--- js/hero.js
+++ js/hero.js
@@ -1,11 +1,32 @@
-// scripts.js
-window.addEventListener('load', function() {
-    const loadingScreen = document.getElementById('loading-screen');
-    const mainContent = document.getElementById('main-content');
-    
-    // Hide the loading screen
-    loadingScreen.style.display = 'none';
-    
-    // Show the main content
-    mainContent.style.display = 'block';
-});
+(function () {
+    "use strict";
+
+    var carousels = function () {
+        $(".owl-carousel").owlCarousel({
+            loop: true,
+            center: true,
+            margin: 0,
+            responsiveClass: true,
+            nav: false,
+            responsive: {
+                0: {
+                    items: 1,
+                    nav: false
+                },
+                680: {
+                    items: 2,
+                    nav: false,
+                    loop: false
+                },
+                1000: {
+                    items: 3,
+                    nav: true
+                }
+            }
+        });
+    };
+
+    (function ($) {
+        carousels();
+    })(jQuery);
+})();
